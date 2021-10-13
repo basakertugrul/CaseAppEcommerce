ac486435e1b7c2bb1e0e8fb3c836a9a16cfd1cc4
basakertugrul <61563492+basakertugrul@users.noreply.github.com>
2021-10-13 18:32:55 +0300

    Commit

--- E-commerce/Model/ProductDataModel.swift
+++ E-commerce/Model/ProductDataModel.swift
@@ -1,8 +1,89 @@
 //
-//  Product.swift
+//  ProductDataModel.swift
 //  E-commerce
 //
 //  Created by Başak Ertuğrul on 13.10.2021.
 //
 
 import Foundation
+import Firebase
+
+
+struct Order {
+    let ref: DatabaseReference?
+    let rowID, shipMode, country, city: String
+    let state, productID, category, subCategory: String
+    let productName, sales, quantity, discount: String
+    let profit: String
+    
+    
+    init(rowID: String, shipMode: String, country: String, city: String, state: String, productID: String , category: String, subCategory: String, productName: String, sales: String, quantity: String, discount: String, profit: String){
+        self.ref = nil
+        self.rowID = rowID
+        self.shipMode = shipMode
+        self.country = country
+        self.city = city
+        self.state = state
+        self.productID = productID
+        self.category = category
+        self.subCategory = subCategory
+        self.productName = productName
+        self.sales = sales
+        self.quantity = quantity
+        self.discount = discount
+        self.profit = profit
+    }
+    
+    init?(snapshot: DataSnapshot){
+        guard
+            let value = snapshot.value as? [String:AnyObject],
+            let rowID = value["Row ID"] as? String,
+            let shipMode = value["Ship Mode"] as? String,
+            let country = value["Country"] as? String,
+            let city = value["City"] as? String,
+            let state = value["State"] as? String,
+            let productID = value["Product ID"] as? String,
+            let category = value["Category"] as? String,
+            let subCategory = value["Sub-Category"] as? String,
+            let productName = value["Product Name"] as? String,
+            let sales = value["Sales"] as? String,
+            let quantity = value["Quantity"] as? String,
+            let discount = value["Discount"] as? String,
+            let profit = value["Profit"] as? String
+        else {
+            return nil
+        }
+        self.ref = snapshot.ref
+        self.rowID = rowID
+        self.shipMode = shipMode
+        self.country = country
+        self.city = city
+        self.state = state
+        self.productID = productID
+        self.category = category
+        self.subCategory = subCategory
+        self.productName = productName
+        self.sales = sales
+        self.quantity = quantity
+        self.discount = discount
+        self.profit = profit
+    }
+    
+    func toAnyObject() -> Any {
+        return [
+            "Row ID": rowID,
+            "Ship Mode": shipMode,
+            "Country": country,
+            "City": city,
+            "State": state,
+            "Product ID": productID,
+            "Category": category,
+            "Sub-Category": subCategory,
+            "Product Name": productName,
+            "Sales": sales,
+            "Quantity": quantity,
+            "Discount": discount,
+            "Profit": profit
+        ]
+    }
+}
--- E-commerce/ViewController/ViewController.swift
+++ E-commerce/ViewController/ViewController.swift
@@ -6,14 +6,30 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
-
+    
+    var lists: [Order] = []
+    var ref: DatabaseReference?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        ref = Database.database().reference(withPath: "Orders")
+        
+        ref?.observe(.value, with: { snapshot in
+            var newLists: [Order] = []
+            for child in snapshot.children {
+                if let snapshot = child as? DataSnapshot,
+                   let newOrder = Order(snapshot: snapshot) {
+                    newLists.append(newOrder)
+                }
+            }
+            self.lists = newLists
+            //            self.tableView.reloadData()
+            print(self.lists)
+        })
     }
-
-
 }
-
